33ab5d3f26b000bcc4d77a62e51d975a1504e426
aline dias necchi ribeiro <alinenecchi@gmail.com>
2021-06-26 15:44:47 -0300

    Configuração de Banco de dados para calculadora de anuncios

---
+++ calculadora.py
@@ -0,0 +1,32 @@
+import sqlite3 
+import os
+
+conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect('anuncios')
+
+# calculo de numeros de visualisações necessarias para 01 click
+VIEW = 12 / 100 
+# calculo de numeros de cliks necessarios para 01 compartilhamento
+CLICK = 3 / 20 
+# 30 pessoas visualizam o anúncio original (não compartilhado) a cada R$ 1,00 investido.
+VIEW_POR_INVESTIMENTO = 30  
+# o mesmo anúncio é compartilhado no máximo 4 vezes em sequência
+MAX_SHARE = 4 
+
+def configure_database():
+    """
+    A função configurar_o_banco tem como objetivo verificar se as tabelas já existem no banco, caso não cria-las, caso
+    sim, chamar a função main para dar incio no sistema!
+    :return:
+    """
+    try:
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE clientes (cpf BIGINT(12) NOT NULL, nome VARCHAR(50)NULL DEFAULT NULL, PRIMARY KEY("
+                       "cpf))")
+        cursor.execute("""CREATE TABLE orcamentos (ID INTEGER PRIMARY KEY AUTOINCREMENT,nome_anuncio VARCHAR(50) NULL 
+           DEFAULT NULL, data_inicio VARCHAR(11) NOT NULL, data_fim VARCHAR(11) NOT NULL, invest_por_dia FLOAT(20) 
+           NOT NULL, invest_total FLOAT(20) NOT NULL, cpf BIGINT(11) NOT NULL, max_view INT(20) NOT NULL, max_clic 
+           INT(20) NOT NULL, max_share INT(20) NOT NULL, FOREIGN KEY(cpf) REFERENCES clientes(cpf))""")
+        conn.commit()
+    except sqlite3.OperationalError:
+
